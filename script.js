c54403c04811ce82bc1ce9427ac5b210ff808fc5
Amisha2912 <amishadsce006@gmail.com>
2025-01-15 00:44:13 +0530

    added functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let toggle =document.querySelector(".dark-mode");
 let toggleBtn=document.querySelector(".toggle-btn");
-
+let categoryList=document.querySelector("#categories");
+let showBook=document.querySelector("#main-view");
+let uniqueSet =new Set();
 //------for Dark-Mode--------
 window.addEventListener('load', function () {
     if (localStorage.getItem('theme') === 'dark') {
@@ -23,3 +25,71 @@ toggle.addEventListener('click', function () {
         localStorage.setItem('theme', 'light'); // Save preference
     }
 });
+
+let getCategory = async ()=>{
+    let response =await fetch('https://books-backend.p.goit.global/books/category-list');
+    let data =await response.json();
+    renderData(data);
+    
+
+    let response1=await fetch('https://books-backend.p.goit.global/books/top-books');
+    let data1=await response1.json();
+    renderData1(data1);
+    // console.log(data1);
+    // data1.forEach((book)=>{
+    //     book.books.forEach(item =>{
+    //         console.log("============");
+    //         console.log(item.book_image);
+    //         console.log(item.list_name);
+    //         console.log(item.author);
+    //         console.log(item.contributor);
+    //         console.log("=============");
+    //     })
+    // })
+}
+getCategory();
+
+categoryList.innerHTML="";
+let renderData =(data)=>{
+    data.forEach(item =>{
+        const list =document.createElement('a');
+        list.classList.add('lists');
+        list.textContent=item.list_name;
+        // console.log(list.textContent);
+        categoryList.appendChild(list);
+    });
+    
+}
+
+
+let renderData1 =((items)=>{
+    items.forEach((book)=>{
+        uniqueSet.add(book.list_name)
+        // console.log(book.list_name);
+        // showBook.innerHTML="book.list_name";
+        book.books.forEach(item =>{
+            let bookItem=document.createElement("div");
+            bookItem.classList.add('book-detail');
+            const img =document.createElement('img');
+            img.src=item.book_image;
+            const title=document.createElement('h3');
+            title.textContent=item.title;
+            const author =document.createElement("h4");
+            author.textContent=item.author;
+            bookItem.appendChild(img);
+            bookItem.appendChild(title);
+            bookItem.appendChild(author);
+            showBook.appendChild(bookItem);
+        })
+    })
+})
+
+
+const filterByList =(listName)=>{
+   let categoryBook = listName.filter(item =>{
+        return item.list_name===uniqueSet;
+    })
+
+    renderData(categoryBook);
+
+}
